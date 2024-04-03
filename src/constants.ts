8360285046d370c84033d1e87c2ff606ada13203
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-03 10:28:04 +0200

    1.0.5

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,6 @@ export const TARGET_DIR = 'src/locales';
 export const EXIT_SUCCESS = 0;
 export const EXIT_FAILURE = 1;
 export const FIRST_ARGUMENT = 2;
+export const SECOND_ARGUMENT = 3;
 export const EMPTY = 0;
 export const DEFAULT_INDENTATION = 2;
